73ef7e3bd20890240a214f05b484423b1e731c4f
Bob Goddard <git@1.git.bgcomp.co.uk>
2020-05-28 12:58:05 +0100

    .

--- src/linux_gpio.adb
+++ src/linux_gpio.adb
@@ -122,11 +122,11 @@ package body Linux_GPIO is
          raise label_exception;
       end if;
 
-      if Ada.Strings.Unbounded.Length (Consumer_Label) < 31 then
-         Request.consumer_label := Interfaces.C.To_C (Ada.Strings.Unbounded.To_String (Consumer_Label) & Blanks_32 ((Ada.Strings.Unbounded.Length (Consumer_Label) + 2) .. Blanks_32'Length));
-      else
+--      if Ada.Strings.Unbounded.Length (Consumer_Label) < 31 then
+--         Request.consumer_label := Interfaces.C.To_C (Ada.Strings.Unbounded.To_String (Consumer_Label) & Blanks_32 ((Ada.Strings.Unbounded.Length (Consumer_Label) + 2) .. Blanks_32'Length));
+--      else
          Request.consumer_label := Interfaces.C.To_C (Ada.Strings.Unbounded.To_String (Consumer_Label));
-      end if;
+--      end if;
 
       fd := fd_type (GNAT.OS_Lib.Open_Read_Write (ldevname, lmode));
 
